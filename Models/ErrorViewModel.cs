025bc13292637f72e1c4d7a809e89e320c283012
Alok-Vaghela <117078632+Alok-Vaghela@users.noreply.github.com>
2024-04-17 11:00:05 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AAAApprovedSignature.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
